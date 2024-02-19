a5bd737b78e5d388c4ed3c4a68f0e43e74681964
ostap larson <ostapphx@gmail.com>
2024-02-20 00:24:56 +0200

    test

--- app/helper/model.py
+++ app/helper/model.py
@@ -86,7 +86,7 @@ class GrafanaAlert(BaseModel):
             text_indent = "    "
             alert_name = self.commonLabels.get("alertname", "")
             time_emote = SkypeMsg.emote("(23f1_stopwatch)")
-            project_emote = SkypeMsg.emote("1f4c4_pagefacingup")
+            project_emote = SkypeMsg.emote("bomb")
             status_emoticon_dict = {
                 "firing": SkypeMsg.emote("bomb"),
                 "resolved": SkypeMsg.emote("smile"),
@@ -95,7 +95,7 @@ class GrafanaAlert(BaseModel):
             emote = status_emoticon_dict.get(self.status, "")
             return (
                 f"{SkypeMsg.bold('Project:')} {self.projectName.upper()} \n"
-                f"{SkypeMsg.bold('Status')}: {self.status.upper()} {time_emote}\n"
+                f"{SkypeMsg.bold('Status')}: {self.status.upper()} {project_emote}\n"
                 f"{SkypeMsg.bold('Info')}\n"
                 f"{join_char.join(textwrap.indent(alert.model_representer(), text_indent) for alert in self.alerts)}\n"
             )
