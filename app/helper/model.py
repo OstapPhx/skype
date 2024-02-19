f35d764e4c3dd5340c34b5e957d82fb1e5bf085f
ostap larson <ostapphx@gmail.com>
2024-02-20 01:55:23 +0200

    test

--- app/helper/model.py
+++ app/helper/model.py
@@ -25,7 +25,7 @@ class Alert(BaseModel):
         return (
             f"{SkypeMsg.bold('Stage')}: {self.labels}\n"
             f"{SkypeMsg.bold('Values')}: {self.value_string_parser()}\n"
-            f"{SkypeMsg.bold('Pipeline date')}: {self.startsAt} {time_emote}\n"
+            f"{time_emote} {SkypeMsg.bold('Pipeline date')}: {self.startsAt}\n"
             f"{SkypeMsg.link(url=self.silenceURL, display='Pipeline URL')}\n"
             f"{SkypeMsg.link(url=self.commitURL, display='Commit URL')}\n"
         )
